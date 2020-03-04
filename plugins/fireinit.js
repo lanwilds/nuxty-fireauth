1e7f80f33524ff874c9aee2462580f9da120ae09
Lanwil DS <lanwildsouza98@gmail.com>
2020-03-04 15:05:34 +0530

    Update fireinit.js

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -5,13 +5,13 @@ import 'firebase/database';
 
 if(!firebase.apps.length){
   firebase.initializeApp({
-    apiKey: "AIzaSyA3WmttYVAQ7amXR2tUGCJFQNS0FS3D3NQ",
-    authDomain: "vue-auth-725b9.firebaseapp.com",
-    databaseURL: "https://vue-auth-725b9.firebaseio.com",
-    projectId: "vue-auth-725b9",
-    storageBucket: "vue-auth-725b9.appspot.com",
-    messagingSenderId: "371120106411",
-    appId: "1:371120106411:web:6b1bfd5f5cd4c58f5840ee"
+    apiKey: "xxxxx",
+    authDomain: "vue-auth-xxxx.firebaseapp.com",
+    databaseURL: "https://vue-auth-xxxx.firebaseio.com",
+    projectId: "vue-auth-xxxx",
+    storageBucket: "vue-auth-xxxx.appspot.com",
+    messagingSenderId: "xxxx",
+    appId: "xxxx"
   });
 }
 
